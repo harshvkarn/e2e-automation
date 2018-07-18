7b0df685685fc1a8a0c1a44b2a91b913bfd4c1be
harshvkarn <harshvkarn54@gmail.com>
2018-07-18 13:27:27 +0530

    Add kops file to provision Google Cloud
    Signed-off-by: harshvkarn <harshvkarn54@gmail.com>

---
+++ kops-gcp/random-name.py
@@ -0,0 +1,6 @@
+import string
+import random
+def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+random=id_generator()
+print ('openebs-e2e-'+random)
